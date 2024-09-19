9baf3d7b273a2fcbda8992dcc6a78656baf01fa3
iamtalhasaleem <iamtalhasaleem911@gmail.com>
2024-09-19 12:05:02 +0500

    Initial commit

---
+++ app/chat/create_embeddings.py
@@ -0,0 +1,34 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import codecs
+
+
+def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
+    """
+    Generate and store embeddings for the given pdf
+
+    1. Extract text from the specified PDF.
+    2. Divide the extracted text into manageable chunks.
+    3. Generate an embedding for each chunk.
+    4. Persist the generated embeddings.
+
+    :param pdf_id: The unique identifier for the PDF.
+    :param pdf_path: The file path to the PDF.
+
+    Example Usage:
+
+    create_embeddings_for_pdf('123456', '/path/to/pdf')
+    """
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 100
+    )
+    
+    loader = PyPDFLoader(pdf_path)
+    
+    docs = loader.load_and_split(text_splitter)
+    
+    # print(docs)
+
+    print(codecs.encode(str(docs), 'utf-8').decode()) 
